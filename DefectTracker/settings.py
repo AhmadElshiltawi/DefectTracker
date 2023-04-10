d9c1a3d6a42f97d1b07e938fffd04b13116011cc
Ahmad <85766582+AhmadElshiltawi@users.noreply.github.com>
2023-04-10 14:00:21 -0600

    Add files via upload

--- DefectTracker/settings.py
+++ DefectTracker/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+# Expire the session when the user closes their browser
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
